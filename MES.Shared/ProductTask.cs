ed9aebbf65c3d73b411369575dc4a3cb543777d9
Dmitry Frolov <frdima19@gmail.com>
2021-03-17 20:34:29 +0700

    change ProductTask

--- MES.Shared/ProductTask.cs
+++ MES.Shared/ProductTask.cs
@@ -18,11 +18,31 @@ namespace MES
 			TimeOnBench = benchTime;
 		}
 
+		public ProductTask (ProductTask task)
+		{
+			Number = task.Number;
+			TimeOnBench = (int[])task.TimeOnBench.Clone();
+			StartTime = (int[])task.StartTime?.Clone();
+		}
+
+		public override int GetHashCode ()
+		{
+			return Number.GetHashCode();
+		}
+
+		public override bool Equals (object obj)
+		{
+			var task = obj as ProductTask;
+			if (task==null)
+				return base.Equals(obj);
+			return task.Number == Number;
+		}
+
 		public override string ToString ()
 		{
 			if ( StartTime != null )
 			{
-				var timeSpans = StartTime.Zip(TimeOnBench, (s, d) => $"{s}-{d}");
+				var timeSpans = StartTime.Zip(TimeOnBench, (s, d) => $"{s}-{s + d}");
 				return Number + String.Format("[{0}]", String.Join(", ", timeSpans));
 			}
 			return Number + String.Format("[{0}]", String.Join(", ", TimeOnBench));
