6890ed21679a6afeeaa660751ebc28a0d74faf05
Dmitry Frolov <frdima19@gmail.com>
2021-03-17 12:29:37 +0700

    make plugin prototype

---
+++ JonsonFirstRule/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MES;
+using MES.Shared;
+
+namespace JonsonFirstRule
+{
+	public class JonsonFirst : IMESPlugin
+	{
+		public string Name => "Jonson first rule";
+
+		public ProductTask[] Apply (ProductTask[] tasks)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
