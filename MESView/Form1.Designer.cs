5f25c9b9ee65733a5292d38c6dee132c785c7045
Dmitry Frolov <frdima19@gmail.com>
2021-04-09 18:26:48 +0700

    finish UI + add datasets

--- MESView/Form1.Designer.cs
+++ MESView/Form1.Designer.cs
@@ -30,14 +30,14 @@
 		{
 			this.tabControl1 = new System.Windows.Forms.TabControl();
 			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.inputPlot = new OxyPlot.WindowsForms.PlotView();
 			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.outputPlot = new OxyPlot.WindowsForms.PlotView();
 			this.evaluateBtn = new System.Windows.Forms.Button();
 			this.methodBox = new System.Windows.Forms.ComboBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
 			this.button1 = new System.Windows.Forms.Button();
-			this.inputPlot = new OxyPlot.WindowsForms.PlotView();
-			this.outputPlot = new OxyPlot.WindowsForms.PlotView();
 			this.tabControl1.SuspendLayout();
 			this.tabPage1.SuspendLayout();
 			this.tabPage2.SuspendLayout();
@@ -45,12 +45,15 @@
 			// 
 			// tabControl1
 			// 
+			this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.tabControl1.Controls.Add(this.tabPage1);
 			this.tabControl1.Controls.Add(this.tabPage2);
-			this.tabControl1.Location = new System.Drawing.Point(205, 3);
+			this.tabControl1.Location = new System.Drawing.Point(232, 0);
 			this.tabControl1.Name = "tabControl1";
 			this.tabControl1.SelectedIndex = 0;
-			this.tabControl1.Size = new System.Drawing.Size(592, 445);
+			this.tabControl1.Size = new System.Drawing.Size(859, 614);
 			this.tabControl1.TabIndex = 0;
 			// 
 			// tabPage1
@@ -59,10 +62,23 @@
 			this.tabPage1.Location = new System.Drawing.Point(4, 25);
 			this.tabPage1.Name = "tabPage1";
 			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage1.Size = new System.Drawing.Size(584, 416);
+			this.tabPage1.Size = new System.Drawing.Size(840, 585);
 			this.tabPage1.TabIndex = 0;
 			this.tabPage1.Text = "Input";
 			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// inputPlot
+			// 
+			this.inputPlot.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.inputPlot.Location = new System.Drawing.Point(3, 3);
+			this.inputPlot.Name = "inputPlot";
+			this.inputPlot.PanCursor = System.Windows.Forms.Cursors.Hand;
+			this.inputPlot.Size = new System.Drawing.Size(834, 579);
+			this.inputPlot.TabIndex = 0;
+			this.inputPlot.Text = "plotView1";
+			this.inputPlot.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+			this.inputPlot.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+			this.inputPlot.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
 			// 
 			// tabPage2
 			// 
@@ -70,32 +86,46 @@
 			this.tabPage2.Location = new System.Drawing.Point(4, 25);
 			this.tabPage2.Name = "tabPage2";
 			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage2.Size = new System.Drawing.Size(584, 416);
+			this.tabPage2.Size = new System.Drawing.Size(851, 585);
 			this.tabPage2.TabIndex = 1;
 			this.tabPage2.Text = "Output";
 			this.tabPage2.UseVisualStyleBackColor = true;
 			// 
+			// outputPlot
+			// 
+			this.outputPlot.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.outputPlot.Location = new System.Drawing.Point(3, 3);
+			this.outputPlot.Name = "outputPlot";
+			this.outputPlot.PanCursor = System.Windows.Forms.Cursors.Hand;
+			this.outputPlot.Size = new System.Drawing.Size(845, 579);
+			this.outputPlot.TabIndex = 1;
+			this.outputPlot.Text = "plotView1";
+			this.outputPlot.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+			this.outputPlot.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+			this.outputPlot.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+			// 
 			// evaluateBtn
 			// 
-			this.evaluateBtn.Location = new System.Drawing.Point(26, 405);
+			this.evaluateBtn.Location = new System.Drawing.Point(15, 119);
 			this.evaluateBtn.Name = "evaluateBtn";
 			this.evaluateBtn.Size = new System.Drawing.Size(75, 23);
 			this.evaluateBtn.TabIndex = 1;
 			this.evaluateBtn.Text = "Calculate";
 			this.evaluateBtn.UseVisualStyleBackColor = true;
+			this.evaluateBtn.Click += new System.EventHandler(this.evaluateBtn_Click);
 			// 
 			// methodBox
 			// 
 			this.methodBox.FormattingEnabled = true;
-			this.methodBox.Location = new System.Drawing.Point(26, 375);
+			this.methodBox.Location = new System.Drawing.Point(15, 89);
 			this.methodBox.Name = "methodBox";
-			this.methodBox.Size = new System.Drawing.Size(121, 24);
+			this.methodBox.Size = new System.Drawing.Size(211, 24);
 			this.methodBox.TabIndex = 2;
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(23, 355);
+			this.label1.Location = new System.Drawing.Point(12, 69);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(55, 17);
 			this.label1.TabIndex = 3;
@@ -107,42 +137,19 @@
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(47, 12);
+			this.button1.Location = new System.Drawing.Point(15, 12);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(100, 36);
 			this.button1.TabIndex = 4;
 			this.button1.Text = "Load data";
 			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// inputPlot
-			// 
-			this.inputPlot.Location = new System.Drawing.Point(17, 31);
-			this.inputPlot.Name = "inputPlot";
-			this.inputPlot.PanCursor = System.Windows.Forms.Cursors.Hand;
-			this.inputPlot.Size = new System.Drawing.Size(550, 369);
-			this.inputPlot.TabIndex = 0;
-			this.inputPlot.Text = "plotView1";
-			this.inputPlot.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
-			this.inputPlot.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
-			this.inputPlot.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
-			// 
-			// outputPlot
-			// 
-			this.outputPlot.Location = new System.Drawing.Point(17, 24);
-			this.outputPlot.Name = "outputPlot";
-			this.outputPlot.PanCursor = System.Windows.Forms.Cursors.Hand;
-			this.outputPlot.Size = new System.Drawing.Size(550, 369);
-			this.outputPlot.TabIndex = 1;
-			this.outputPlot.Text = "plotView1";
-			this.outputPlot.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
-			this.outputPlot.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
-			this.outputPlot.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(1093, 616);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.methodBox);
@@ -150,6 +157,7 @@
 			this.Controls.Add(this.tabControl1);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
 			this.tabControl1.ResumeLayout(false);
 			this.tabPage1.ResumeLayout(false);
 			this.tabPage2.ResumeLayout(false);
--- MESView/Form1.cs
+++ MESView/Form1.cs
@@ -3,18 +3,109 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using MES.Shared;
 
 namespace MES.View
 {
 	public partial class Form1 : Form
 	{
+		List<IMESPlugin> plugins;
+		ProductTask[] inputData;
+		ProductTask[] resultData;
+
 		public Form1 ()
 		{
 			InitializeComponent();
+		}
+
+		private void button1_Click (object sender, EventArgs e)
+		{
+			if ( openFileDialog1.ShowDialog() == DialogResult.OK )
+			{
+				string path = openFileDialog1.FileName;
+				inputData = readTasks(path);
+				Utils.SetStartingTimes(inputData);
+				plotData(inputPlot, inputData);
+			}
+		}
+
+		private void plotData (PlotView plot, IEnumerable<ProductTask> tasks)
+		{
+			var lastTask = tasks.Last();
+			var totalTime = lastTask.StartTime.Last() + lastTask.TimeOnBench.Last();
+			var model = new PlotModel
+			{
+				Title = "Total time = " + totalTime,
+				LegendPlacement = LegendPlacement.Outside,
+				LegendPosition = LegendPosition.LeftTop,
+			};
+
+			foreach ( var task in tasks )
+			{
+				var taskSeries = new RectangleBarSeries { Title = "Task" + task.Number };
+				for ( int i = 0; i < task.TimeOnBench.Length; i++ )
+				{
+					taskSeries.Items.Add(new RectangleBarItem
+					{
+						X0 = task.StartTime[i],
+						X1 = task.StartTime[i] + task.TimeOnBench[i],
+						Y0 = 0 + i,
+						Y1 = 1 + i
+					});
+
+				}
+				model.Series.Add(taskSeries);
+			}
+			plot.Model = model;
+		}
+
+		static int parseInt (string str) => str == "" ? 0 : int.Parse(str);
+
+		private ProductTask[] readTasks (string path)
+		{
+			TextReader reader = new StreamReader(path, Encoding.Default);
+			var table = CSVService.ReadTable(reader, parseInt, true);
+
+			ProductTask[] productTasks = new ProductTask[table.Length];
+
+			for ( int i = 0; i < table.Length; i++ )
+			{
+				var row = table[i];
+				var times = row.Skip(1).ToArray();
+				var number = row[0];
+				productTasks[i] = new ProductTask(number, times);
+			}
+			return productTasks;
+		}
+
+		private void Form1_Load (object sender, EventArgs e)
+		{
+			var loader = new PluginLoader();
+			loader.LoadPlugins();
+
+			plugins = PluginLoader.Plugins;
+			plugins.Sort((l, r) => l.Name.CompareTo(r.Name));
+
+			methodBox.Items.AddRange(plugins.ToArray());
+			methodBox.SelectedIndex = 0;
+		}
+
+		private void evaluateBtn_Click (object sender, EventArgs e)
+		{
+			var plugin = methodBox.SelectedItem as IMESPlugin;
+			if ( plugin == null || inputData == null )
+				return;
+
+			resultData = plugin.Apply(inputData);
+			plotData(outputPlot, resultData);
+			tabControl1.SelectedIndex = 1;
 		}
 	}
 }
