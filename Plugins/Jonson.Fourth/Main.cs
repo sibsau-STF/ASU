1e1fa37cfe12cff2ff248ad3f93d4169424d2f35
Dmitry Frolov <frdima19@gmail.com>
2021-04-11 21:05:50 +0700

    fix jonson bottleneck rule

--- Plugins/Jonson.Fourth/Main.cs
+++ Plugins/Jonson.Fourth/Main.cs
@@ -14,32 +14,11 @@ namespace Jonson
 
 		public ProductTask[] Apply (ProductTask[] tasks)
 		{
-			var sorted = _farthestBottleNeck(tasks).ToArray();
+			int indexOfBottleNeck (ProductTask task) => task.TimeOnBench.IndexOf(task.TimeOnBench.Max());
+
+			var sorted = Utils.SortedTasks(tasks, indexOfBottleNeck, false).ToArray();
 			Utils.SetStartingTimes(sorted);
 			return sorted;
-		}
-
-		private static IEnumerable<ProductTask> _farthestBottleNeck (IEnumerable<ProductTask> tasks)
-		{
-			var taskStack = tasks.ToList();
-			var cols = tasks.First().TimeOnBench.Length;
-			List<ProductTask> result = new List<ProductTask>();
-
-			for ( int c = cols - 1; c > 1; c-- )
-			{
-				// получаю время выполнение каждой детали на n, n-1.. станке
-				var times = taskStack.Select(tsk => tsk.TimeOnBench[c]).ToList();
-
-				//перемещаю элемент с максимальным временем выполнения в результат
-				var maxTime = times.Max();
-				var index = times.IndexOf(maxTime);
-				result.Add(taskStack[index]);
-				taskStack.RemoveAt(index);
-			}
-			// сортирую задачи по времени выполнения на втором станке
-			taskStack.Sort((one, two) => -1 * one.TimeOnBench[1].CompareTo(two.TimeOnBench[1]));
-			result.AddRange(taskStack);
-			return result;
 		}
 
 		public override string ToString () => Name;
